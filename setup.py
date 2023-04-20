a34721e266c4c39f8abd5037ea83c2ed774d9606
Rodrigo Antônio de Araújo <rodjjo@gmail.com>
2023-04-20 15:42:13 -0300

    Add setup requires

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
     install_requires=['numpy', 'scipy', 'matplotlib'],
-
+    setup_requires=['numpy', 'scipy', 'matplotlib'],
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
